93dbb071923daec170d050b33cc7f04c39b1c2be
A601-22 <tnk.dat.class@gmail.com>
2020-09-10 11:46:18 +0900

    画面デザイン

--- yoketoruvs20/Form1.Designer.cs
+++ yoketoruvs20/Form1.Designer.cs
@@ -29,12 +29,154 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.titlelabel = new System.Windows.Forms.Label();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.copyrightlabel = new System.Windows.Forms.Label();
+            this.timelabel = new System.Windows.Forms.Label();
+            this.starlabel = new System.Windows.Forms.Label();
+            this.highscorelabel = new System.Windows.Forms.Label();
+            this.gameoverlabel = new System.Windows.Forms.Label();
+            this.crearlabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titlelabel
+            // 
+            this.titlelabel.AutoSize = true;
+            this.titlelabel.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.titlelabel.Location = new System.Drawing.Point(258, 140);
+            this.titlelabel.Name = "titlelabel";
+            this.titlelabel.Size = new System.Drawing.Size(360, 62);
+            this.titlelabel.TabIndex = 0;
+            this.titlelabel.Text = "よけとる2020";
+            this.titlelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titlelabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // startbutton
+            // 
+            this.startbutton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.startbutton.Font = new System.Drawing.Font("MS UI Gothic", 40F);
+            this.startbutton.Location = new System.Drawing.Point(340, 254);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(197, 101);
+            this.startbutton.TabIndex = 1;
+            this.startbutton.Text = "スタート";
+            this.startbutton.UseVisualStyleBackColor = false;
+            // 
+            // copyrightlabel
+            // 
+            this.copyrightlabel.AutoSize = true;
+            this.copyrightlabel.Font = new System.Drawing.Font("MS UI Gothic", 15F);
+            this.copyrightlabel.Location = new System.Drawing.Point(12, 466);
+            this.copyrightlabel.Name = "copyrightlabel";
+            this.copyrightlabel.Size = new System.Drawing.Size(310, 20);
+            this.copyrightlabel.TabIndex = 6;
+            this.copyrightlabel.Text = "Copyright © 2020 Tacky-Yamamoto";
+            this.copyrightlabel.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // timelabel
+            // 
+            this.timelabel.AutoSize = true;
+            this.timelabel.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timelabel.Location = new System.Drawing.Point(11, 9);
+            this.timelabel.Name = "timelabel";
+            this.timelabel.Size = new System.Drawing.Size(114, 30);
+            this.timelabel.TabIndex = 7;
+            this.timelabel.Text = "Time 100";
+            this.timelabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // starlabel
+            // 
+            this.starlabel.AutoSize = true;
+            this.starlabel.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.starlabel.Location = new System.Drawing.Point(11, 34);
+            this.starlabel.Name = "starlabel";
+            this.starlabel.Size = new System.Drawing.Size(63, 30);
+            this.starlabel.TabIndex = 8;
+            this.starlabel.Text = "★:10";
+            // 
+            // highscorelabel
+            // 
+            this.highscorelabel.AutoSize = true;
+            this.highscorelabel.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.highscorelabel.Location = new System.Drawing.Point(328, 202);
+            this.highscorelabel.Name = "highscorelabel";
+            this.highscorelabel.Size = new System.Drawing.Size(221, 31);
+            this.highscorelabel.TabIndex = 9;
+            this.highscorelabel.Text = "HighScore 100";
+            // 
+            // gameoverlabel
+            // 
+            this.gameoverlabel.AutoSize = true;
+            this.gameoverlabel.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.gameoverlabel.Location = new System.Drawing.Point(290, 140);
+            this.gameoverlabel.Name = "gameoverlabel";
+            this.gameoverlabel.Size = new System.Drawing.Size(296, 62);
+            this.gameoverlabel.TabIndex = 10;
+            this.gameoverlabel.Text = "がめおヴぇｒ";
+            this.gameoverlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.gameoverlabel.Visible = false;
+            // 
+            // crearlabel
+            // 
+            this.crearlabel.AutoSize = true;
+            this.crearlabel.Font = new System.Drawing.Font("UD デジタル 教科書体 NK-B", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.crearlabel.Location = new System.Drawing.Point(336, 140);
+            this.crearlabel.Name = "crearlabel";
+            this.crearlabel.Size = new System.Drawing.Size(204, 62);
+            this.crearlabel.TabIndex = 11;
+            this.crearlabel.Text = "くりあー";
+            this.crearlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.crearlabel.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 30F);
+            this.button1.Location = new System.Drawing.Point(340, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 101);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "タイトルへ\r\n戻る";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(877, 495);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.crearlabel);
+            this.Controls.Add(this.gameoverlabel);
+            this.Controls.Add(this.highscorelabel);
+            this.Controls.Add(this.starlabel);
+            this.Controls.Add(this.timelabel);
+            this.Controls.Add(this.copyrightlabel);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.titlelabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titlelabel;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label copyrightlabel;
+        private System.Windows.Forms.Label timelabel;
+        private System.Windows.Forms.Label starlabel;
+        private System.Windows.Forms.Label highscorelabel;
+        private System.Windows.Forms.Label gameoverlabel;
+        private System.Windows.Forms.Label crearlabel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -16,5 +16,30 @@ namespace yoketoruvs20
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
